9b5896b5ea4eca150e26623cb2b3fdf83e3571df
Ruslan <rus.zakirov.2000@bk.ru>
2022-06-26 01:05:11 +0700

    Cont create gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch } = require("gulp");
 const gulpSass = require("gulp-sass")(require("sass"));
 const gulpSourceMaps = require("gulp-sourcemaps");
 const gulpUglify = require("gulp-uglify");
-const browserSync = require("browser-sync");
+const browserSync = require("browser-sync").create();
 const gulpConcat = require("gulp-concat");
 
 const parseSASS = () => {
@@ -13,5 +13,25 @@ const watchParseSASS = () => {
   watch("src/*.scss", parseSASS);
 };
 
+const initBrowserSync = () => {
+  browserSync.init({
+    server: {
+      baseDir: "public/",
+    },
+  });
+};
+
+const parseJS = () => {
+  return src("src/*.js")
+    .pipe(gulpSourceMaps.init())
+    .pipe(gulpConcat("bundle.js"))
+    .pipe(gulpUglify())
+    .pipe(gulpSourceMaps.write("."))
+    .pipe(dest("res"));
+};
+
 exports.parseSASS = parseSASS;
 exports.watchParseSASS = watchParseSASS;
+
+exports.initBrowserSync = initBrowserSync;
+exports.parseJS = parseJS;
