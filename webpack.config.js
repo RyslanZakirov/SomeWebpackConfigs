adc9660141175c060d2d2732a4d27c7ebad3405e
Ruslan <rus.zakirov.2000@bk.ru>
2022-06-26 00:40:40 +0700

    Create base webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,26 @@
 const path = require("path");
 
-module.exports = {};
+module.exports = {
+  entry: path.join(__dirname, "source", "index.jsx"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "result"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
